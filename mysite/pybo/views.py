dfdc52662c06079980fbecebdec9c256031a0dd2
lee3jjang <lee3jjang@gmail.com>
2021-02-14 16:27:18 +0900

    회원가입

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -2,6 +2,7 @@ from django.utils import timezone
 from django.http import HttpResponse
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Question
 from .forms import QuestionForm
@@ -22,20 +23,25 @@ def detail(request, question_id):
     return render(request, 'pybo/question_detail.html', context)
     # return HttpResponse(question_id)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     question.answer_set.create(
         content=request.POST.get('content'),
-        create_date=timezone.now()
+        create_date=timezone.now(),
+        author=request.user,
     )
+
     return redirect('pybo:detail', question_id=question_id)
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == "POST":
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
             question.create_date = timezone.now()
+            question.author = request.user
             question.save()
             return redirect('pybo:index')
     else:
